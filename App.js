ee71687cadf1ff20583fd9552fcf0c5b2235f536
morbitzerd <d.morbitzer@techdivision.com>
2023-04-02 15:20:24 +0200

    SH-7: Add Enviorment Variable for api url

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
+import Constants from "expo-constants";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const apiUrl = Constants.expoConfig.extra.apiUrl;
+  console.log(apiUrl);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
