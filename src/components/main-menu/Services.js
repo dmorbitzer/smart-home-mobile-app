fc30a3d9aeaff3adfe0eadf89e5d258b0d36e507
morbitzerd <d.morbitzer@techdivision.com>
2023-04-21 10:20:37 +0200

    SH-27: Refactor getIcon

--- src/components/main-menu/Services.js
+++ src/components/main-menu/Services.js
@@ -1,24 +1,9 @@
-import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import GetIcon from "../../api/GetIcon";
 import { Box } from "@react-native-material/core";
 
 import IconButton from "../util/IconButton";
 
 const servicesList = [];
-const size = 25;
-const getIcon = (title) => {
-  switch (title) {
-    case "Katzenprofil":
-      return () => <Icon name="cat" color="black" size={size} />;
-    case "Katzenfütterung":
-      return () => <Icon name="food" color="black" size={size} />;
-    case "Katzenerkennung":
-      return () => (
-        <Icon name="smoke-detector-variant" color="black" size={size} />
-      );
-    default:
-      return "";
-  }
-};
 
 export default function Services(props) {
   for (let i = 0; i < props.data.services.edges.length; i++) {
@@ -28,7 +13,7 @@ export default function Services(props) {
           key={props.data.services.edges[i].node.name}
           title={props.data.services.edges[i].node.name}
           type="primary"
-          icon={getIcon(props.data.services.edges[i].node.name)}
+          icon={GetIcon(props.data.services.edges[i].node.name)}
           func={() =>
             props.navigation.navigate(props.data.services.edges[i].node.name)
           }
