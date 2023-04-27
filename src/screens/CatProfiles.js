3949b7ecae581fbb41aed93c9876a3a818bd9e1e
morbitzerd <d.morbitzer@techdivision.com>
2023-04-27 16:51:40 +0200

    SH-13: Change profileList generation to array.map

--- src/screens/CatProfiles.js
+++ src/screens/CatProfiles.js
@@ -1,39 +1,38 @@
-import { Stack } from "@react-native-material/core";
-import { useState } from "react";
-import { RefreshControl, ScrollView, StyleSheet } from "react-native";
+import {Stack} from "@react-native-material/core";
+import {useState} from "react";
+import {RefreshControl, ScrollView, StyleSheet} from "react-native";
 
-import { GetCatProfiles } from "../api/CatProfiles";
+import {GetCatProfiles} from "../api/CatProfiles";
 import Profiles from "../components/cat-profiles/Profiles";
 import Loading from "../components/util/Loading";
 
-export default function CatProfiles({ navigation }) {
-  const [refreshing, setRefreshing] = useState(false);
-  const getData = GetCatProfiles();
+export default function CatProfiles({navigation}) {
+    const [refreshing, setRefreshing] = useState(false);
+    const getData = GetCatProfiles();
 
-  const onRefresh = () => {
-    setRefreshing(true);
-    getData.refetch();
-    setTimeout(() => {
-      setRefreshing(false);
-    }, 2000);
-  };
+    const onRefresh = () => {
+        setRefreshing(true);
+        getData.refetch();
+        setRefreshing(false);
+    };
 
-  if (getData.loading) {
-    return <Loading />;
-  }
-  return (
-    <Stack m={4} spacing={2}>
-      <ScrollView style={styles.catProfileList}>
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh}>
-          <Profiles data={getData.data} navigation={navigation} />
-        </RefreshControl>
-      </ScrollView>
-    </Stack>
-  );
+    if (getData.loading) {
+        return <Loading/>;
+    }
+    console.log(getData.data.cats.edges);
+    return (
+        <Stack m={4} spacing={2}>
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh}>
+                <ScrollView style={styles.catProfileList}>
+                    <Profiles data={getData.data.cats.edges} navigation={navigation}/>
+                </ScrollView>
+            </RefreshControl>
+        </Stack>
+    );
 }
 
 const styles = StyleSheet.create({
-  catProfileList: {
-    height: "90%",
-  },
+    catProfileList: {
+        height: "90%",
+    },
 });
