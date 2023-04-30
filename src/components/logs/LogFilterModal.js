2fb7c4caf57b6deea183b32ff0668022cd417625
morbitzerd <d.morbitzer@techdivision.com>
2023-04-30 22:30:08 +0200

    SH-38: Add margin to switch

--- src/components/logs/LogFilterModal.js
+++ src/components/logs/LogFilterModal.js
@@ -131,6 +131,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
+    marginTop: 10,
   },
   switch: {
     marginLeft: 10,
