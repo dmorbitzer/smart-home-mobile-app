d2dfb7884cc678d387060fd6fffd8e3d0773db14
morbitzerd <d.morbitzer@techdivision.com>
2023-04-30 20:27:44 +0200

    SH-15: Change date in tests

--- tests/components/cat-profiles/Details.test.js
+++ tests/components/cat-profiles/Details.test.js
@@ -9,7 +9,7 @@ const localData = {
     cat: {
       id: "/api/cats/245",
       name: "Bla",
-      birthdate: "2023-04-14T00:00:00+02:00",
+      birthdate: "2023-04-14",
       gender: {
         name: "male",
       },
--- tests/components/cat-profiles/DetailsTable.test.js
+++ tests/components/cat-profiles/DetailsTable.test.js
@@ -8,7 +8,7 @@ const props = {
     cat: {
       id: "/api/cats/245",
       name: "Bla",
-      birthdate: "2023-04-14T00:00:00+02:00",
+      birthdate: "2023-04-14",
       gender: {
         name: "male",
       },
