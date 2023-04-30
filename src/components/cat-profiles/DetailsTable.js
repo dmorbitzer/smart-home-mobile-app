803d5c0d00b80e0cc38519749221797e70207fc3
morbitzerd <d.morbitzer@techdivision.com>
2023-04-30 20:25:08 +0200

    SH-15: Fix Date

--- src/components/cat-profiles/DetailsTable.js
+++ src/components/cat-profiles/DetailsTable.js
@@ -2,10 +2,6 @@ import { Text, HStack, VStack } from "@react-native-material/core";
 
 export default function DetailsTable(props) {
   const date = new Date(props.data.cat.birthdate);
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
-  const localBirthdate = day + "." + month + "." + year;
   const localGender =
     props.data.cat.gender.name === "male" ? "Männlich" : "Weiblich";
   return (
@@ -19,7 +15,7 @@ export default function DetailsTable(props) {
       <VStack fill center spacing={4} divider>
         <Text variant="h6">{localGender}</Text>
         <Text variant="h6">{props.data.cat.race}</Text>
-        <Text variant="h6">{localBirthdate}</Text>
+        <Text variant="h6">{date.toLocaleDateString("de-DE")}</Text>
         <Text variant="h6">{props.data.cat.weight} kg</Text>
       </VStack>
     </HStack>
