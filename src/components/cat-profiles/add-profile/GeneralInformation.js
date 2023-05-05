775e9040059c51b093cc0af16ac469150be31b5d
morbitzerd <d.morbitzer@techdivision.com>
2023-05-05 15:29:53 +0200

    SH-14: Finalise addcat screen

--- src/components/cat-profiles/add-profile/GeneralInformation.js
+++ src/components/cat-profiles/add-profile/GeneralInformation.js
@@ -2,16 +2,24 @@ import { TextInput, Text } from "@react-native-material/core";
 import { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import DropDownPicker from "react-native-dropdown-picker";
-import { useTheme } from "react-native-paper";
+import NumericInput from "react-native-numeric-input";
+import { HelperText, useTheme } from "react-native-paper";
 import { DatePickerInput } from "react-native-paper-dates";
 
 export default function GeneralInformation(props) {
   const theme = useTheme();
   const [open, setOpen] = useState(false);
-  const [genderItems, setGenderItems] = useState([
-    { label: "Weiblich", value: "female" },
-    { label: "Männlich", value: "male" },
-  ]);
+  const genderItems = props.genders.map((element) => {
+    return {
+      label:
+        element.node.name === "male"
+          ? "Männlich"
+          : element.node.name === "female"
+          ? "Weiblich"
+          : "",
+      value: element.node.id,
+    };
+  });
 
   return (
     <View>
@@ -23,32 +31,45 @@ export default function GeneralInformation(props) {
         value={props.catName}
         style={styles.input}
       />
+      <HelperText type="error" visible={props.hasCatNameError}>
+        Katzennahme fehlt!
+      </HelperText>
 
       <Text style={styles.label}>Geburtsdatum:</Text>
-      <DatePickerInput
-        locale="de"
-        label={null}
-        value={props.catBirthdate}
-        onChange={props.setCatBirthdate}
-        inputMode="start"
-        color="red"
-        inputEnabled={false}
-        style={styles.input}
-      />
+      <View style={styles.input}>
+        <DatePickerInput
+          locale="de"
+          label={null}
+          value={props.catBirthdate}
+          onChange={props.setCatBirthdate}
+          inputMode="start"
+          color="red"
+          inputEnabled={false}
+        />
+      </View>
 
-      <Text style={styles.label}>Geschlecht:</Text>
-      <DropDownPicker
-        open={open}
-        value={props.catSex}
-        items={genderItems}
-        setOpen={setOpen}
-        setValue={props.setCatSex}
-        setItems={setGenderItems}
-        placeholder="Geschlecht wählen"
-        containerStyle={styles.input}
-        style={{ backgroundColor: theme.colors.background }}
-        listItemContainerStyle={{ backgroundColor: theme.colors.background }}
-      />
+      <HelperText type="error" visible={props.hasBirthdateError}>
+        Geburtsdatum fehlt!
+      </HelperText>
+
+      <View style={styles.dropdownView}>
+        <Text style={styles.label}>Geschlecht:</Text>
+        <DropDownPicker
+          open={open}
+          value={props.catSex}
+          items={genderItems}
+          setOpen={setOpen}
+          setValue={props.setCatSex}
+          placeholder="Geschlecht wählen"
+          containerStyle={styles.input}
+          style={{ backgroundColor: theme.colors.background }}
+          listItemContainerStyle={{ backgroundColor: theme.colors.background }}
+          listMode="SCROLLVIEW"
+        />
+        <HelperText type="error" visible={props.hasCatSexError}>
+          Katzengeschlecht fehlt!
+        </HelperText>
+      </View>
 
       <Text style={styles.label}>Rasse:</Text>
       <TextInput
@@ -58,6 +79,26 @@ export default function GeneralInformation(props) {
         value={props.catRace}
         style={styles.input}
       />
+      <HelperText type="error" visible={props.hasCatRaceError}>
+        Katzenrasse fehlt!
+      </HelperText>
+
+      <Text style={styles.label}>Gewicht in Kilogram:</Text>
+      <NumericInput
+        value={props.catWeight}
+        minValue={0}
+        maxValue={10}
+        onChange={(value) => props.setCatWeight(value)}
+        containerStyle={styles.input}
+        valueType="real"
+        totalWidth={150}
+        totalHeight={50}
+        iconSize={25}
+        step={0.1}
+      />
+      <HelperText type="error" visible={props.hasCatWeightError}>
+        Katzengewicht in Kilogram fehlt!
+      </HelperText>
     </View>
   );
 }
@@ -65,10 +106,12 @@ export default function GeneralInformation(props) {
 const styles = StyleSheet.create({
   input: {
     marginTop: 20,
-    marginBottom: 20,
   },
   label: {
     marginTop: 20,
     fontWeight: "bold",
+  },
+  dropdownView: {
+    zIndex: 20,
   },
 });
--- src/screens/AddCat.js
+++ src/screens/AddCat.js
@@ -2,24 +2,123 @@ import { Divider, Stack } from "@react-native-material/core";
 import { useState } from "react";
 import { ScrollView, StyleSheet } from "react-native";
 
-import GeneralInformation from "../components/cat-profiles/add-profile/AddProfileGeneralInformation";
-import IntelligentWaitingTime from "../components/cat-profiles/add-profile/AddProfileIntelligentWaitingTime";
+import { useAddCatProfile } from "../api/CatProfiles";
+import { useGetGenders } from "../api/Gender";
 import GetIcon from "../api/GetIcon";
+import GeneralInformation from "../components/cat-profiles/add-profile/GeneralInformation";
 import IconButton from "../components/util/IconButton";
+import Loading from "../components/util/Loading";
 
 export default function AddCat({ navigation }) {
+  const addCat = useAddCatProfile();
+  const genders = useGetGenders();
+
+  const [loading, setLoading] = useState(false);
+
+  const [catName, setCatName] = useState("");
+  const [catBirthdate, setCatBirthdate] = useState(null);
+  const [catSex, setCatSex] = useState(null);
+  const [catRace, setCatRace] = useState("");
+  const [catWeight, setCatWeight] = useState(0);
+
+  const [hasCatNameError, setHasCatNameError] = useState(false);
+  const [hasBirthdateError, setHasBirthdateError] = useState(false);
+  const [hasCatSexError, setHasCatSexError] = useState(false);
+  const [hasCatRaceError, setHasCatRaceError] = useState(false);
+  const [hasCatWeightError, setHasCatWeightError] = useState(false);
+
+  const validate = () => {
+    if (catName.length === 0) {
+      setHasCatNameError(true);
+    } else {
+      setHasCatNameError(false);
+    }
+
+    if (catBirthdate === null) {
+      setHasBirthdateError(true);
+    } else {
+      setHasBirthdateError(false);
+    }
+
+    if (catSex === null) {
+      setHasCatSexError(true);
+    } else {
+      setHasCatSexError(false);
+    }
+
+    if (catRace.length === 0) {
+      setHasCatRaceError(true);
+    } else {
+      setHasCatSexError(false);
+    }
+
+    if (catWeight === 0) {
+      setHasCatWeightError(true);
+    } else {
+      setHasCatWeightError(false);
+    }
+
+    return !(
+      catName.length === 0 ||
+      catBirthdate === null ||
+      catSex === null ||
+      catRace.length === 0 ||
+      catWeight === 0
+    );
+  };
+
+  const saveCat = async () => {
+    if (!validate()) {
+      return;
+    }
+    setLoading(true);
+    const response = await addCat(
+      catName,
+      catBirthdate,
+      catWeight,
+      catRace,
+      catSex
+    );
+    setLoading(false);
+    if (response) {
+      navigation.navigate("Katzenprofil", {
+        refresh: true,
+      });
+    }
+  };
+
+  if (genders.loading || loading) {
+    return <Loading />;
+  }
+
   return (
-    <ScrollView style={styles.addCatView}>
+    <ScrollView style={styles.addCatView} nestedScrollEnabled>
       <Stack spacing={2} style={{ margin: 32 }}>
-        <GeneralInformation />
-        <Divider style={styles.divider}/>
-        <IntelligentWaitingTime />
+        <GeneralInformation
+          genders={genders.data.genders.edges}
+          catName={catName}
+          setCatName={setCatName}
+          catBirthdate={catBirthdate}
+          setCatBirthdate={setCatBirthdate}
+          catSex={catSex}
+          setCatSex={setCatSex}
+          catWeight={catWeight}
+          setCatWeight={setCatWeight}
+          catRace={catRace}
+          setCatRace={setCatRace}
+          hasCatNameError={hasCatNameError}
+          hasBirthdateError={hasBirthdateError}
+          hasCatSexError={hasCatSexError}
+          hasCatWeightError={hasCatWeightError}
+          hasCatRaceError={hasCatRaceError}
+        />
+        <Divider style={styles.divider} />
         <IconButton
-            key={1}
-            title="Speichern"
-            type="primary"
-            func={() => console.log("test")}
-            icon={GetIcon("Speichern")}
+          key={1}
+          title="Speichern"
+          type="primary"
+          func={saveCat}
+          icon={GetIcon("Speichern")}
         />
       </Stack>
     </ScrollView>
@@ -32,5 +131,5 @@ const styles = StyleSheet.create({
   },
   divider: {
     marginTop: 30,
-  }
+  },
 });
